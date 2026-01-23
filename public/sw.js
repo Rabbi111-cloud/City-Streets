3de63447a92a410cb53b0d6cb4f6c9d8f7e1a26d
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-01-23 09:33:32 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'naija-streets-v1'
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll(['/','/dashboard'])
+    )
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  )
+})
